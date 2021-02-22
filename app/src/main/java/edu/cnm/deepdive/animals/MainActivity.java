881549d5e5b782bc103e4defbb7e25f50d741c77
Hana Dawson <stubbornunicorn95@gmail.com>
2021-02-22 10:55:37 -0700

    Spinner functional.

--- app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
@@ -1,10 +1,13 @@
 package edu.cnm.deepdive.animals;
 
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,59 +21,71 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-        private WebView contentView;
-        private Spinner animalSelector;
+  private WebView contentView;
+  private Spinner animalSelector;
+  private ArrayAdapter<Animal> adapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        contentView = findViewById(R.id.content_view);
-        animalSelector = findViewById(R.id.animal_selector);
-        setupWebView();
-    }
-    private void setupWebView() {
-        contentView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                return false;
-            }
-        }); //zoom in/out controls
-        WebSettings settings = contentView.getSettings();
-        settings.setSupportZoom(true);
-        settings.setBuiltInZoomControls(true);
-        settings.setDisplayZoomControls(false);
-        settings.setUseWideViewPort(true);
-        settings.setLoadWithOverviewMode(true);
-        new Retriever().start();
-    }
-
-    private class Retriever extends Thread {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    contentView = findViewById(R.id.content_view);
+    animalSelector = findViewById(R.id.animal_selector);
+    animalSelector.setOnItemSelectedListener(new OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Animal animal = (Animal) parent.getItemAtPosition(position);
+            contentView.loadUrl(animal.getImageUrl());
+        }
 
         @Override
-        public void run() {
-            try {
-                Response<List<Animal>> response = WebServiceProxy.getInstance()
-                    .getAnimals(BuildConfig.API_KEY)
-                    .execute();
-                if (response.isSuccessful()) {
-                    List<Animal> animals = response.body();
-                    Random rng = new Random();
-                    //noinspection ConstantConditions
-                    String url = animals.get(rng.nextInt(animals.size())).getImageUrl();
-                    runOnUiThread(() -> {
-                        contentView.loadUrl(url);
-                        ArrayAdapter<Animal> adapter = new ArrayAdapter<>(
-                            MainActivity.this, android.R.layout.simple_dropdown_item_1line, animals);
-                        animalSelector.setAdapter(adapter);
-                    });
-                } else {
-                    Log.e(getClass().getName(), response.message());
-                }
-            } catch (IOException e) {
-                Log.e(getClass().getName(), e.getMessage(), e);
-            }
-
+        public void onNothingSelected(AdapterView<?> parent) {
         }
+    });
+    setupWebView();
+  }
+
+  private void setupWebView() {
+    contentView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+      }
+    }); //zoom in/out controls
+    WebSettings settings = contentView.getSettings();
+    settings.setSupportZoom(true);
+    settings.setBuiltInZoomControls(true);
+    settings.setDisplayZoomControls(false);
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
+    new Retriever().start();
+  }
+
+  private class Retriever extends Thread {
+
+    @Override
+    public void run() {
+      try {
+        Response<List<Animal>> response = WebServiceProxy.getInstance()
+            .getAnimals(BuildConfig.API_KEY)
+            .execute();
+        if (response.isSuccessful()) {
+          List<Animal> animals = response.body();
+          //noinspection ConstantConditions
+          String url = animals.get(0).getImageUrl();
+          adapter = new ArrayAdapter<>(MainActivity.this, R.layout.item_animal_spinner, animals);
+          adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+          runOnUiThread(() -> {
+            contentView.loadUrl(url);
+            animalSelector.setAdapter(adapter);
+          });
+        } else {
+          Log.e(getClass().getName(), response.message());
+        }
+      } catch (IOException e) {
+        Log.e(getClass().getName(), e.getMessage(), e);
+      }
+
     }
+  }
 }
